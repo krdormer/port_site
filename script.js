ba56c798ea95c18c483182f9ffb15942d25d3b06
krdormer <kevin.dormer18@yahoo.com>
2020-10-10 11:10:19 -0500

    adding in dark/light functionality SVG

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
+const image4 = document.getElementById('image4');
+const image5 = document.getElementById('image5');
+
 // const textBox = document.getElementById('text-box');
 
 // Dark or Light Images 
@@ -11,6 +14,8 @@ function imageMode(color) {
     image1.src = `img/undraw_proud_coder_${color}.svg`;
     image2.src = `img/undraw_feeling_proud_${color}.svg`;
     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+    image4.src = `img/meditate${color}.svg`;
+    image5.src = `img/yoga${color}.svg`;
 }
 
 function toggleDarkLightMode(isDark){
