c9127ae4a4d890ad68cb4e4b113b6a69e0a8d488
krdormer <krdormer.dev@gmail.com>
2021-02-08 11:20:39 -0600

    Updating project image content to update with light/dark modes

--- src/themeSwitch.js
+++ src/themeSwitch.js
@@ -9,6 +9,10 @@ const image4 = document.getElementById('image4');
 const image5 = document.getElementById('image5');
 const image6 = document.getElementById('image6');
 
+const projectImageOne = document.getElementById('projectImageOne');
+const projectImageTwo = document.getElementById('projectImageTwo');
+const projectImageThree = document.getElementById('projectImageThree');
+
 // const textBox = document.getElementById('text-box');
 
 // Dark or Light Images 
@@ -19,9 +23,13 @@ function imageMode(color) {
     image4.src = `../src/img/biking_${color}.svg`;
     image5.src = `../src/img/meditation_${color}.svg`;
     image6.src = `../src/img/yoga_${color}.svg`;
+
+    projectImageOne.src = `../src/img/shopping_${color}.svg`;
+    projectImageTwo.src = `../src/img/pip_${color}.svg`;
+    projectImageThree.src = `../src/img/jtb_${color}.svg`;
 }
 
-function toggleDarkLightMode(isDark){
+function toggleDarkLightMode(isDark) {
     nav.style.backgroundColor = isDark ? 'rgb(0 0 0 /50%)' : 'rgb(255 255 255 / 50%)'
     // textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 /50%)'
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode'
@@ -49,7 +57,7 @@ toggleSwitch.addEventListener('change', switchTheme);
 // Check Local Storage for Theme 
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme',  currentTheme);
+    document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
         toggleDarkLightMode(true);
